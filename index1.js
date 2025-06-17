78a6616f3a8375913697cb2e1d08dcd88fed9025
Deepak Dewatwal <136556140+Dewatwal@users.noreply.github.com>
2025-06-17 16:55:22 +0530

    modified

--- index1.js
+++ index1.js
@@ -1,19 +1,20 @@
-const parent = React.createElement("div",{id:"parent"},
-   React.createElement("div",{id:"child"},[
-    React.createElement("h1",{id:"heading1"},"How are you"),
-    React.createElement("h2",{id:"heading2"},"I am fine")]
-    
-   )
-);
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading1" }, "How are you"),
+    React.createElement("h2", { id: "heading2" }, "I am fine"),
+  ])
+); // this is react.createElement, through which we can write react in js , now in next videos we will se how jsx works
 
 const element = React.createElement(
-  'button',
-  { 
-    onClick: () => alert('Clicked!'),
-    className: 'btn'
+  "button",
+  {
+    onClick: () => alert("Clicked!"),
+    className: "btn",
   },
-  'Click Me'
+  "Click Me"
 );
-      const root = ReactDOM.createRoot(document.getElementById("root"));
-      root.render(parent);
-      console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+console.log(parent);
